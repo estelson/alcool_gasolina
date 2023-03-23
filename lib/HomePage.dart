9cb0729d594ed3413a3070a8f87262f87c0a4ad2
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 18:36:30 -0300

    Criando a tela principal do app e suas funções

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,13 +8,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controllerAlcool = TextEditingController();
+  final TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular() {
+    double? precoAlcool = double.tryParse(_controllerAlcool.text);
+    double? precoGasolina = double.tryParse(_controllerGasolina.text);
+
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado = "Número inválido. Digite números maiores que 0 e utilizando (.)";
+      });
+    } else {
+      /*
+      * Se o preço do álcool divido pelo preço da gasolina
+      * for >= a 0.7 é melhor abastecer com gasolina
+      * senão é melhor utilizar álcool
+      * */
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina";
+        });
+      } else {
+        setState(() {
+          _textoResultado = "Melhor abastecer com álcool";
+        });
+      }
+
+      _limparCampos();
+    }
+  }
+
+  void _limparCampos() {
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Álcool ou Gasolina"),
+        backgroundColor: Colors.blue,
+      ),
       body: Container(
+        padding: const EdgeInsets.all(32),
         child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: Image.asset("assets/images/logo.png"),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecer o seu carro",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: "Preço Álcool, ex: 4.39"),
+                style: const TextStyle(fontSize: 22),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: "Preço Gasolina, ex: 5.64"),
+                style: const TextStyle(fontSize: 22),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: _calcular,
+                  child: const Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
