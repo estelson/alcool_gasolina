85ed8b50f11044e3f4905d4ae5264b65d4870a16
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 18:22:03 -0300

    Initial commit
    Aplicação baseada no curso Udemy - Desenvolvimento Android e IOS com Flutter - Crie 15 Apps - https://www.udemy.com/course/desenvolvimento-android-e-ios-com-flutter - Seção 11, Aula 72

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:alcool_gasolina/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
